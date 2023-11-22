3a0dcf038194a76dc68dcc2427e980eb0f98c8c4
OlegPichkur <oleg1pichkur@gmail.com>
2023-11-22 13:27:20 +0200

    частично js

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,79 @@
-/*-----------Функции-------------*/
-/*---------События------------*/
-const burger = document.querySelector('.burger')
-const vertMenu = document.querySelector('.vert-menu')
-burger.addEventListener('click', () => {
-   // burger.style.display = "none"
-   // vertMenu.style.display = "block"
-   // console.log('dddddd');
-   burger.style.cssText += `
-      display: none;
-   `
-   vertMenu.style.cssText += `
-      display: block;
-   `
-}, false)
+"use strict"
+/* -------Определение устройства-------*/ 
+const isMobile = {
+   Android: function() {
+      return navigator.userAgent.match(/Android/i);
+   },
+   BlackBerry: function() {
+      return navigator.userAgent.match(/BlackBerry/i);
+   },
+   iOS: function() {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+   },
+   Opera: function() {
+      return navigator.userAgent.match(/Opera Mini/i);
+   },
+   Windows: function() {
+      return navigator.userAgent.match(/IEMobile/i);
+   },
+   any: function() {
+      return (
+         isMobile.Android() ||
+         isMobile.BlackBerry() ||
+         isMobile.iOS() ||
+         isMobile.Opera() ||
+         isMobile.Windows()
+      )
+   }
+};
+if (isMobile.any()) {
+   document.body.classList.add('_touch')
+} else {
+   document.body.classList.add('_pc')
+}
+/*-----Адаптация ширины горизонтального меню*/
+const widthWindow = document.documentElement.clientWidth
+const headerMenu = document.querySelector('.header__nav')
+window.addEventListener('DOMContentLoaded', adaptWidthMenu)
 
-
+function adaptWidthMenu() {
+   if (widthWindow < 1440 && widthWindow > 1110) {
+      let padding = ((widthWindow-1110) / 2)+'px'
+      headerMenu.style.cssText += `
+      padding-left: ${padding};
+      padding-right: ${padding};
+      `
+   }
+}
+/*------Прокрутка при клике------*/
+const menuLinks = document.querySelectorAll('.menu__link[data-goTo]')
+if(menuLinks.length > 0) {
+   menuLinks.forEach(menuLink => {
+      menuLink.addEventListener('click', onMenuLinkClick)
+   });
+   function onMenuLinkClick(e) {
+      const menuLink = e.target;
+      if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.     goto)) {
+         const gotoBlock = document.querySelector(menuLink.dataset.goto)
+         const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header__nav').offsetHeight; 
+         window.scrollTo({
+            top: gotoBlockValue,
+            behavior: "smooth"
+         });
+         e.preventDefault();
+      }
+   }
+}
+/*---------Меню-бургер----------*/
+const burgerMenu = document.querySelector('.burger')
+if(burgerMenu) {
+   const vertMenu = document.querySelector('.vert-menu')
+   console.log(vertMenu);
+   burgerMenu.addEventListener('click', function() {
+   
+      burgerMenu.classList.toggle('_active')
+      vertMenu.classList.toggle('_active')
+      
+   }, false)
  
+}
