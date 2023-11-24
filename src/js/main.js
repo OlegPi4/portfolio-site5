be19306f88b173f3449956d36ac5e2e268fff93a
OlegPichkur <oleg1pichkur@gmail.com>
2023-11-24 23:52:17 +0200

    2411

--- src/js/main.js
+++ src/js/main.js
@@ -34,9 +34,10 @@ if (isMobile.any()) {
 /*-----Адаптация ширины горизонтального меню*/
 const widthWindow = document.documentElement.clientWidth
 const headerMenu = document.querySelector('.header__nav')
-window.addEventListener('DOMContentLoaded', adaptWidthMenu)
+window.addEventListener('DOMContentLoaded', adaptWidthMenu())
 
 function adaptWidthMenu() {
+   
    if (widthWindow < 1440 && widthWindow > 1110) {
       let padding = ((widthWindow-1110) / 2)+'px'
       headerMenu.style.cssText += `
@@ -65,15 +66,18 @@ if(menuLinks.length > 0) {
    }
 }
 /*---------Меню-бургер----------*/
-const burgerMenu = document.querySelector('.burger')
-if(burgerMenu) {
-   const vertMenu = document.querySelector('.vert-menu')
-   console.log(vertMenu);
-   burgerMenu.addEventListener('click', function() {
+
+// const burgerMenu = document.querySelector('.burger')
+// if(burgerMenu) {
+//    const vertMenu = document.querySelector('.vert-menu')
+//    burgerMenu.addEventListener("click", function(e) {
+//       document.body.classList.toggle('_lock')
+//       burgerMenu.classList.toggle('_active')
+//       vertMenu.classList.toggle('_active')
+//    })
    
-      burgerMenu.classList.toggle('_active')
-      vertMenu.classList.toggle('_active')
-      
-   }, false)
- 
+// }
+let burg = document.querySelector('.burger')
+burg.onclick = function() {
+   burg.classList.toggle('_active')
 }
