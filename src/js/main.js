3c87686d3f591024c9ba874ceb216e498fda65ec
OlegPichkur <oleg1pichkur@gmail.com>
2023-11-25 22:18:24 +0200

    format script

--- src/js/main.js
+++ src/js/main.js
@@ -27,35 +27,38 @@ const isMobile = {
    }
 };
 if (isMobile.any()) {
-   document.body.classList.add('_touch')
+   document.body.classList.add('_touch');
 } else {
-   document.body.classList.add('_pc')
+   document.body.classList.add('_pc');
 }
 /*-----Адаптация ширины горизонтального меню*/
-const widthWindow = document.documentElement.clientWidth
-const headerMenu = document.querySelector('.header__nav')
-window.addEventListener('DOMContentLoaded', adaptWidthMenu())
+const widthWindow = document.documentElement.clientWidth;
+const headerMenu = document.querySelector('.header__nav');
+window.addEventListener('DOMContentLoaded', adaptWidthMenu());
 
 function adaptWidthMenu() {
    
-   if (widthWindow < 1440 && widthWindow > 1110) {
-      let padding = ((widthWindow-1110) / 2)+'px'
+   if ( widthWindow < 1440 && widthWindow > 1110 ) {
+      let padding = ((widthWindow-1110) / 2)+'px';
       headerMenu.style.cssText += `
       padding-left: ${padding};
       padding-right: ${padding};
-      `
+      `;
    }
+
 }
 /*------Прокрутка при клике------*/
-const menuLinks = document.querySelectorAll('.menu__link[data-goTo]')
-if(menuLinks.length > 0) {
+const menuLinks = document.querySelectorAll('.menu__link[data-goTo]');
+
+if ( menuLinks.length > 0 ) {
    menuLinks.forEach(menuLink => {
       menuLink.addEventListener('click', onMenuLinkClick)
    });
    function onMenuLinkClick(e) {
       const menuLink = e.target;
-      if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.     goto)) {
-         const gotoBlock = document.querySelector(menuLink.dataset.goto)
+
+      if ( menuLink.dataset.goto && document.querySelector(menuLink.dataset.    goto)) {
+         const gotoBlock = document.querySelector(menuLink.dataset.goto);
          const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header__nav').offsetHeight; 
          window.scrollTo({
             top: gotoBlockValue,
@@ -63,20 +66,18 @@ if(menuLinks.length > 0) {
          });
          e.preventDefault();
       }
+
    }
 }
 /*---------Меню-бургер----------*/
 
 const burgerMenu = document.querySelector('.burger')
+
 if(burgerMenu) {
    const vertMenu = document.querySelector('.vert-menu')
    burgerMenu.addEventListener("click", function(e) {
-      document.body.classList.toggle('_lock')
-      burgerMenu.classList.toggle('_active')
-      vertMenu.classList.toggle('_active')
+      document.body.classList.toggle('_lock');
+      burgerMenu.classList.toggle('_active');
+      vertMenu.classList.toggle('_active');
    })
 }
-// let burg = document.querySelector('.burger')
-// burg.onclick = function() {
-//    burg.classList.toggle('_active')
-// }
